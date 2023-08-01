ae9130cd9c7d1476ac8fa29202f0c0af7ec88108
KamiliArsyad <arsyad.ik@gmail.com>
2023-08-01 09:08:40 +0800

    init 3d wrapper

---
+++ Visualizer.h
@@ -0,0 +1,10 @@
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Surface_mesh.h>
+#include <CGAL/alpha_wrap_3.h>
+#include <CGAL/IO/read_points.h>
+#include <CGAL/Real_timer.h>
+#include <iostream>
+#include <string>
+#include <pthread.h>
+
+namespace AW3 = CGAL::Alpha_wraps_3;
